1a5df0c9da7fb2f8cb50b20adfe578779bcbf90d
DriHut <63098227+DriHut@users.noreply.github.com>
2021-11-17 14:32:17 +0100

    fix bound of button class

--- Brick breaker/Button.cpp
+++ Brick breaker/Button.cpp
@@ -10,7 +10,7 @@ Button::Button(int x, int y, int w, int h) {
 Button::~Button() {};
 
 bool Button::isHovered(SDL_Point cursor) {
-	if (cursor.y > box.y && cursor.x < box.x + box.w && cursor.y > box.y && cursor.y < box.y + box.h) {
+	if (cursor.x > box.x && cursor.x < box.x + box.w && cursor.y > box.y && cursor.y < box.y + box.h) {
 		is_hovered = true;
 	} else {
 		is_hovered = false;
--- Brick breaker/LabeledButton.h
+++ Brick breaker/LabeledButton.h
@@ -8,7 +8,7 @@ private:
 	SDL_Texture* texture_hover;
 public:
 	LabeledButton(const char* text, int x, int y, TTF_Font* font, SDL_Color color, SDL_Color hover_color, SDL_Renderer* renderer);
-	~LabeledButton();
+	~LabeledButton() {};
 
 	void setHoverTexture(const char* text, TTF_Font* font, SDL_Color color, SDL_Renderer* renderer);
 	void render(SDL_Renderer* renderer);
